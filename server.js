d38cf2265b6e84432967132df8b66bfb157d6b57
Aakash <aakashsrivastava008@gmail.com>
2021-06-24 13:31:48 +0530

    getting ready for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 const db = require('./config/mongoose');
 const routes = require('./routes');
 const expressLayouts = require('express-ejs-layouts');
